8b1ba781d54ec5bf78f4dab771d187d9fd572ca1
LiaoMingRong <849376723@qq.com>
2023-01-28 19:04:03 +0800

    dbeug

--- scripts/background.js
+++ scripts/background.js
@@ -1,5 +1,75 @@
 const api_url = 'https://shop.admin.cn/';
 const expire_time = 24 * 60 * 60; //缓存时间
+const SOCKET = {
+    init: function(type, callback) {
+        const _this = this;
+        if (_this.ioLoginSign) {
+            return;
+        }
+        getCache('helper_all_data_cache', function(config) {
+            if (config) {
+                _this.type = type;
+                _this.ioLoginSign = false;
+                // _this.socket = new WebSocket(config.socket_domain);
+                _this.socket = new WebSocket('wss://shop.admin.cn/socket.io/');
+
+            } else {
+                callback({ code: 400, data: false, msg: '无配置数据'});
+            }
+        });
+    },
+    logout: function(type, callback) {
+
+    },
+    set: function(param, callback) {
+
+    },
+    ioPing: function() {
+        const _this = this;
+        clearInterval(_this.ioPingHandler);
+        _this.ioPingHandler = setInterval(function() {
+            if (_this.ioLoginSign) {
+                _this.sendMessage('ioPing', 'ping');
+            } else {
+                clearInterval(_this.ioPingHandler);
+            }
+        }, 20000);
+    },
+};
+const CACHE = {
+    getCache: function(key, callback) {
+        let data = localStorage.getItem(key);
+        if (!data) {
+            callback(false);
+        } else {
+            if (typeof data === 'string') {
+                data = JSON.parse(data);
+            }
+            if (data.expire == -1) {
+                callback(data.content);
+            } else if (data.expire <= this.getTime()) {
+                this.delCache(key);
+                callback(false);
+            } else {
+                callback(data.content);
+            }
+        }
+    },
+    setCache: function(key, value, expire, callback) {
+        if (expire != -1) {
+            expire = this.getTime() + (expire ? expire : expire_time);
+        }
+        localStorage.setItem(key, JSON.stringify({expire: expire, content: value}));
+        if (callback) callback(true);
+    },
+    delCache: function(key, callback) {
+        localStorage.removeItem(key);
+        if (callback) callback(true);
+    },
+    getTime: function() {
+        return parseInt(new Date().getTime() / 1000);
+    }
+};
 //扩展内通信
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
@@ -95,71 +165,4 @@ function getApi(url, param, callback, type) {
         callback({ code: 500, msg: error.message});
     });
 }
-const SOCKET = {
-    init: function(type, callback) {
-        const _this = this;
-        if (_this.ioLoginSign) {
-            return;
-        }
-        getCache('helper_all_data_cache', function(config) {
-            if (config) {
-                _this.type = type;
-                _this.ioLoginSign = false;
-                _this.socket = new WebSocket(config.socket_domain);
-            } else {
-                callback({ code: 400, data: false, msg: '无配置数据'});
-            }
-        });
-    },
-    logout: function(type, callback) {
-
-    },
-    set: function(param, callback) {
-
-    },
-    ioPing: function() {
-        const _this = this;
-        clearInterval(_this.ioPingHandler);
-        _this.ioPingHandler = setInterval(function() {
-            if (_this.ioLoginSign) {
-                _this.sendMessage('ioPing', 'ping');
-            } else {
-                clearInterval(_this.ioPingHandler);
-            }
-        }, 20000);
-    },
-};
-const CACHE = {
-    getCache: function(key, callback) {
-        let data = localStorage.getItem(key);
-        if (!data) {
-            callback(false);
-        } else {
-            if (typeof data === 'string') {
-                data = JSON.parse(data);
-            }
-            if (data.expire == -1) {
-                callback(data.content);
-            } else if (data.expire <= this.getTime()) {
-                this.delCache(key);
-                callback(false);
-            } else {
-                callback(data.content);
-            }
-        }
-    },
-    setCache: function(key, value, expire, callback) {
-        if (expire != -1) {
-            expire = this.getTime() + (expire ? expire : expire_time);
-        }
-        localStorage.setItem(key, JSON.stringify({expire: expire, content: value}));
-        if (callback) callback(true);
-    },
-    delCache: function(key, callback) {
-        localStorage.removeItem(key);
-        if (callback) callback(true);
-    },
-    getTime: function() {
-        return parseInt(new Date().getTime() / 1000);
-    }
-};
+SOCKET.init();
